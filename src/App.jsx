8d78114f599e6c87c641d08202271a9ae9f06142
northbot <54906390+willWallace-RIT@users.noreply.github.com>
2026-05-01 11:16:20 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <div>
+      <h1>Agri-Nutrition Sim</h1>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
